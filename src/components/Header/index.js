41c99fd9a39802b8a9fafad11dc1daf6f69f279d
unknown <cahuzac.p@gmail.com>
2021-08-17 17:11:45 +0200

    fixed css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import "../../styles/header.scss";
-import "../../styles/burger-menu.scss";
 
-const Header = ({ onClickHomeBTN }) => {
+const Header = () => {
   return (
     <div className="header">
       <Link to="/" className="header-title">
